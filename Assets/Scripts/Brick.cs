e8b910698e4c5fd79e9c3cb6269e41c6c1298892
Unknown <mertenj1@hawkmail.newpaltz.edu>
2017-03-16 10:07:29 -0400

    Uploading Game
    Uploading game to github

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int strength = 1;
+   
+    
+    // Use this for initialization
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        strength--;
+        if (strength == 0) { 
+
+        
+        {
+                this.gameObject.SetActive(false);
+                FindObjectOfType<Ball>().YouBrokeABrick();
+            }
+        }
+    }
+
+}
+
